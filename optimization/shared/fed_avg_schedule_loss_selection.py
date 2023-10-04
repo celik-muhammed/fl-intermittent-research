98052d3d9d341de210567ea09df3f925759ea7eb
Muhammed ÇELİK <94930605+celik-muhammed@users.noreply.github.com>
2023-10-04 10:25:56 +0300

    Updated

--- optimization/shared/fed_avg_schedule_loss_selection.py
+++ optimization/shared/fed_avg_schedule_loss_selection.py
@@ -532,7 +532,7 @@ def build_fed_avg_process(
 
     @tff.tf_computation(at_server_type, client_output_type)
     def accumulate_loss(u,t):
-      value = tf.reshape(tf.math.reduce_sum(t.model_output['loss']), shape = [1,1])
+      value = tf.reshape(tf.math.reduce_sum(t.model_output.loss), shape = [1,1])
       index = t.client_id
       new_u = tf.tensor_scatter_nd_update(u,index,value)  
       return new_u
